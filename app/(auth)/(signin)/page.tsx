9b5d2403af278bc87787ecb168c3a3fbecb06e9c
CodeWithUmair <codewithumair867@gmail.com>
2024-04-09 09:45:39 +0500

    first commit

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -5,8 +5,8 @@ import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
-  title: "Authentication",
-  description: "Authentication forms built using the components.",
+  title: "Properti",
+  description: "Properti",
 };
 
 export default function AuthenticationPage() {
@@ -36,16 +36,15 @@ export default function AuthenticationPage() {
           >
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg>
-          Logo
+          Properti
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo;
+              &ldquo;We get it - there’s no such thing as a slow day in property
+              management.&rdquo;
             </p>
-            <footer className="text-sm">Sofia Davis</footer>
+            {/* <footer className="text-sm">Unknown</footer> */}
           </blockquote>
         </div>
       </div>
@@ -75,7 +74,7 @@ export default function AuthenticationPage() {
             >
               Privacy Policy
             </Link>
-            .
+            <div className=""></div>
           </p>
         </div>
       </div>
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -9,10 +9,7 @@ export default function Header() {
     <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
       <nav className="h-14 flex items-center justify-between px-4">
         <div className="hidden lg:block">
-          <Link
-            href={"https://github.com/Kiranism/next-shadcn-dashboard-starter"}
-            target="_blank"
-          >
+          <Link className="flex" href="/">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -25,6 +22,7 @@ export default function Header() {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
+            Properti
           </Link>
         </div>
         <div className={cn("block lg:!hidden")}>
